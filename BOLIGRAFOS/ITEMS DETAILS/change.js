543eb3e5eb1cff844dd5e4999c9ac4a8440f0683
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-19 11:11:39 -0600

    crear js donde se hara el cambio de color del item

---
+++ BOLIGRAFOS/ITEMS DETAILS/change.js
@@ -0,0 +1,11 @@
+document.getElementById('black').addEventListener('click', function() {
+    window.location.href = 'item1.html'; // Cambia 'nuevoArchivo.html' por la ruta de tu archivo
+});
+
+document.getElementById('blue').addEventListener('click', function() {
+    window.location.href = 'item11.html'; // Cambia 'nuevoArchivo.html' por la ruta de tu archivo
+});
+
+document.getElementById('red').addEventListener('click', function() {
+    window.location.href = 'item12.html'; // Cambia 'nuevoArchivo.html' por la ruta de tu archivo
+});
