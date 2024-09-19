61ecfffacd3714595ff3a80dc17137b2fdaad638
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-19 11:56:24 -0600

    editar js

--- BOLIGRAFOS/ITEMS DETAILS/change.js
+++ BOLIGRAFOS/ITEMS DETAILS/change.js
@@ -1,19 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     // ITEM 1
-
-
-    // ITEM 2
-    document.getElementById('black2').addEventListener('click', function() {
-        window.location.href = 'item2.html'; 
+    document.getElementById('black1').addEventListener('click', function() {
+        window.location.href = 'item1.html'; 
     });
 
-    document.getElementById('red2').addEventListener('click', function() {
-        window.location.href = 'item2red.html'; 
+    document.getElementById('red1').addEventListener('click', function() {
+        window.location.href = 'item1red.html'; 
     });
 
-    document.getElementById('blue2').addEventListener('click', function() {
-        window.location.href = 'item2blue.html';
+    document.getElementById('blue1').addEventListener('click', function() {
+        window.location.href = 'item1blue.html';
     });
-
-
 });
