81f6201cbe8a49843ff36762ad34b60738a8177c
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-19 11:57:36 -0600

    crear js item2

---
+++ BOLIGRAFOS/ITEMS DETAILS/change2.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+   // ITEM 2
+    document.getElementById('black2').addEventListener('click', function() {
+        window.location.href = 'item2.html'; 
+    });
+
+    document.getElementById('red2').addEventListener('click', function() {
+        window.location.href = 'item2red.html'; 
+    });
+
+    document.getElementById('blue2').addEventListener('click', function() {
+        window.location.href = 'item2blue.html';
+    });
+});
