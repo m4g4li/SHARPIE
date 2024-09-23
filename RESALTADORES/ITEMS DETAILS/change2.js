8b4a3e5346a2727e6151e24c30a1aec2e79ba7dc
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-23 16:23:19 -0600

    editar

--- RESALTADORES/ITEMS DETAILS/change2.js
+++ RESALTADORES/ITEMS DETAILS/change2.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
    // ITEM 2
-    document.getElementById('black2').addEventListener('click', function() {
+    document.getElementById('yellow2').addEventListener('click', function() {
         window.location.href = 'item2.html'; 
     });
 
-    document.getElementById('red2').addEventListener('click', function() {
-        window.location.href = 'item2red.html'; 
+    document.getElementById('orange2').addEventListener('click', function() {
+        window.location.href = 'item2orange.html'; 
     });
 
-    document.getElementById('blue2').addEventListener('click', function() {
-        window.location.href = 'item2blue.html';
-    });
 });
