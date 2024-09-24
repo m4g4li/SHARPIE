7c1ca317e62c0ec17817e23f2a44e706f42c8d14
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-23 20:04:20 -0600

    editar

---
+++ RESALTADORES/ITEMS DETAILS/change4.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ITEM 1
+    document.getElementById('yellow4').addEventListener('click', function() {
+        window.location.href = 'item4.html'; 
+    });
+
+    document.getElementById('pastel4').addEventListener('click', function() {
+        window.location.href = 'item4pastel.html'; 
+    });
+});
