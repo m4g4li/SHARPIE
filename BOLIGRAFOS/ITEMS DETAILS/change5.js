190c063221547f967c81006be7984918fd5225e6
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-23 20:59:53 -0600

    editar

---
+++ BOLIGRAFOS/ITEMS DETAILS/change5.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ITEM 2
+     document.getElementById('silver5').addEventListener('click', function() {
+         window.location.href = 'item5.html'; 
+     });
+ 
+     document.getElementById('gold5').addEventListener('click', function() {
+         window.location.href = 'item5gold.html'; 
+     });
+ 
+     document.getElementById('cereza5').addEventListener('click', function() {
+         window.location.href = 'item5red.html';
+     });
+ });
