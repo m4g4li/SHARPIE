da831a38ee76049a44fdd8f36acf7ff61ced3210
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-23 20:53:45 -0600

    editar

---
+++ BOLIGRAFOS/ITEMS DETAILS/change4.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ITEM 2
+     document.getElementById('malva4').addEventListener('click', function() {
+         window.location.href = 'item4.html'; 
+     });
+ 
+     document.getElementById('morado4').addEventListener('click', function() {
+         window.location.href = 'item4v1.html'; 
+     });
+ 
+     document.getElementById('celeste4').addEventListener('click', function() {
+         window.location.href = 'item4v2.html';
+     });
+
+     document.getElementById('cielo4').addEventListener('click', function() {
+        window.location.href = 'item4v3.html';
+    });
+ });
