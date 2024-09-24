6b13d7190b792047692cce60f509faf5f1c876ab
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-23 20:44:35 -0600

    editar

---
+++ BOLIGRAFOS/ITEMS DETAILS/change3.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ITEM 2
+     document.getElementById('black3').addEventListener('click', function() {
+         window.location.href = 'item3.html'; 
+     });
+ 
+     document.getElementById('red3').addEventListener('click', function() {
+         window.location.href = 'item3red.html'; 
+     });
+ 
+     document.getElementById('blue3').addEventListener('click', function() {
+         window.location.href = 'item3blue.html';
+     });
+ });
