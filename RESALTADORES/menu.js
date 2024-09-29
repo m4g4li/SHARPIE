cd2d54e54306d411fe3dee78e098e3bab7d25894
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-28 19:36:01 -0600

    editar

--- RESALTADORES/menu.js
+++
@@ -1,8 +0,0 @@
-let inicio = document.querySelector('.inicio');
-        let menutoggle = document.querySelector('.toggle');
-        menutoggle.onclick = function(){
-            menutoggle.classList.toggle('active');
-            inicio.classList.toggle('active');
-        }
-
-
