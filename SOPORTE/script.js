a8477f04c9a3c7cb0b1371f0b75175d86fc6a006
M4G4LI <magali.berganza@catolica.edu.sv>
2024-09-30 17:39:16 -0600

    agregar seccion soporte

---
+++ SOPORTE/script.js
@@ -0,0 +1,12 @@
+
+function toggleContact (Argentina, Colombia, Espania, Chile, Mexico, USA, Canada, UK, sw, Germany, Italy, France){
+        var element=document.getElementById(Argentina, Colombia, Espania, Chile, Mexico, USA, Canada, UK, sw, Germany, Italy, France);
+        if (element.style.display === "none" || element.style.display === "") {
+        
+            element.style.display = "block";
+            } else {
+                    element.style.display = "none";
+        
+            }
+        
+}
